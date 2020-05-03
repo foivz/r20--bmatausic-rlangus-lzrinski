709c1d81666c78d66971d4ea7e99aea2e53da368
Zrinski34 <61742121+Zrinski34@users.noreply.github.com>
2020-05-03 21:59:05 +0200

    Add files via upload

---
+++ Software/Filmovi.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    public class Filmovi
+    {
+        public static List<Filmovi> lista;
+        public int ID { get; set; }
+        public string Naziv { get; set; }
+        public DateTime Datum { get; set; }
+        public string Cijena { get; set; }
+        public string Dvorana { get; set; }
+
+        public Filmovi(int id, string naziv, DateTime datum, string cijena, string dvorana)
+        {
+            ID = id;
+            Naziv = naziv;
+            Datum = datum;
+            Cijena = cijena;
+            Dvorana = dvorana;
+        }
+
+        public static void LoadList()
+        {
+            lista = new List<Filmovi>();
+            lista.Add(new Filmovi(0, "Brzi i žestoki 1", new DateTime(2020, 1, 25), " 25 kn ", " Dvorana 1 "));
+            lista.Add(new Filmovi(1, "Brzi i žestoki 2", new DateTime(2020, 2, 13), " 25 kn ", " Dvorana 5 "));
+            lista.Add(new Filmovi(2, "Brzi i žestoki 3", new DateTime(2020, 2, 17), " 30 kn ", " Dvorana 4 "));
+            lista.Add(new Filmovi(3, "Brzi i žestoki 4", new DateTime(2020, 2, 17), " 27 kn ", " Dvorana 3 "));
+            lista.Add(new Filmovi(4, "Brzi i žestoki 5", new DateTime(2020, 1, 13), " 25 kn ", " Dvorana 5 "));
+            lista.Add(new Filmovi(5, "Robin Hood", new DateTime(2020, 2, 13), " 35 kn ", " Dvorana 2 "));
+        }
+    }
+}
---
+++ Software/Pregled_Filmova.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace test
+{
+    partial class Pregled_Filmova
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pregledFilmova = new System.Windows.Forms.DataGridView();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnOdjava = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pregledFilmova)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pregledFilmova
+            // 
+            this.pregledFilmova.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.pregledFilmova.Location = new System.Drawing.Point(11, 81);
+            this.pregledFilmova.Margin = new System.Windows.Forms.Padding(2);
+            this.pregledFilmova.Name = "pregledFilmova";
+            this.pregledFilmova.RowHeadersWidth = 51;
+            this.pregledFilmova.RowTemplate.Height = 24;
+            this.pregledFilmova.Size = new System.Drawing.Size(452, 213);
+            this.pregledFilmova.TabIndex = 0;
+            this.pregledFilmova.CellMouseDoubleClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.tablicaData_CellMouseDoubleClick);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CalendarMonthBackground = System.Drawing.Color.Gray;
+            this.dateTimePicker1.Location = new System.Drawing.Point(11, 322);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(2);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(205, 20);
+            this.dateTimePicker1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.button1.Location = new System.Drawing.Point(350, 322);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 41);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Filtriraj";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.button2.Location = new System.Drawing.Point(232, 322);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(113, 41);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Početni prikaz";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Algerian", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(116, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(255, 30);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Pregled filmova ";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Silver;
+            this.pictureBox1.Image = global::test.Properties.Resources.Cinemapic;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(65, 64);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnOdjava
+            // 
+            this.btnOdjava.Location = new System.Drawing.Point(448, 12);
+            this.btnOdjava.Name = "btnOdjava";
+            this.btnOdjava.Size = new System.Drawing.Size(75, 23);
+            this.btnOdjava.TabIndex = 6;
+            this.btnOdjava.Text = "Odjava";
+            this.btnOdjava.UseVisualStyleBackColor = true;
+            // 
+            // Pregled_Filmova
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(535, 424);
+            this.Controls.Add(this.btnOdjava);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.pregledFilmova);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Pregled_Filmova";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pregledFilmova)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView pregledFilmova;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnOdjava;
+    }
+}
+
