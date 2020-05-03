5d0d7096a6ade63bb87fce2d7af1455b12afe7d1
Zrinski34 <61742121+Zrinski34@users.noreply.github.com>
2020-05-03 22:28:40 +0200

    Add files via upload

---
+++ Software/Luka_Zrinski_Pregled_Filmova/Informacije_o_filmu.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace test
+{
+    partial class Informacije_o_filmu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.ID = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Naziv = new System.Windows.Forms.Label();
+            this.Sadrzaj = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Dvorana = new System.Windows.Forms.Label();
+            this.cboxVrijemePrikazivanja = new System.Windows.Forms.ComboBox();
+            this.lblCijena = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.label1.Location = new System.Drawing.Point(19, 118);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Film:";
+            // 
+            // ID
+            // 
+            this.ID.AutoSize = true;
+            this.ID.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.ID.Location = new System.Drawing.Point(121, 148);
+            this.ID.Name = "ID";
+            this.ID.Size = new System.Drawing.Size(76, 22);
+            this.ID.TabIndex = 1;
+            this.ID.Text = "Redatelj";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.label3.Location = new System.Drawing.Point(19, 145);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Redatelj:";
+            // 
+            // Naziv
+            // 
+            this.Naziv.AutoSize = true;
+            this.Naziv.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.Naziv.Location = new System.Drawing.Point(85, 121);
+            this.Naziv.Name = "Naziv";
+            this.Naziv.Size = new System.Drawing.Size(54, 22);
+            this.Naziv.TabIndex = 3;
+            this.Naziv.Text = "Naziv";
+            // 
+            // Sadrzaj
+            // 
+            this.Sadrzaj.Enabled = false;
+            this.Sadrzaj.Location = new System.Drawing.Point(16, 279);
+            this.Sadrzaj.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Sadrzaj.Multiline = true;
+            this.Sadrzaj.Name = "Sadrzaj";
+            this.Sadrzaj.Size = new System.Drawing.Size(720, 239);
+            this.Sadrzaj.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(239, 11);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(345, 46);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Informacije o filmu";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::test.Properties.Resources.Cinemapic;
+            this.pictureBox2.Location = new System.Drawing.Point(16, 10);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(87, 79);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.label4.Location = new System.Drawing.Point(19, 249);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 25);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Sadržaj:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.label5.Location = new System.Drawing.Point(19, 170);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(90, 25);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Vrijeme:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.label6.Location = new System.Drawing.Point(19, 224);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(99, 25);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Dvorana:";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.button1.Location = new System.Drawing.Point(783, 11);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 38);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Odjava ";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.button2.Location = new System.Drawing.Point(783, 449);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(153, 57);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Rezerviraj kartu";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Dvorana
+            // 
+            this.Dvorana.AutoSize = true;
+            this.Dvorana.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.Dvorana.Location = new System.Drawing.Point(121, 228);
+            this.Dvorana.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Dvorana.Name = "Dvorana";
+            this.Dvorana.Size = new System.Drawing.Size(78, 22);
+            this.Dvorana.TabIndex = 14;
+            this.Dvorana.Text = "Dvorana";
+            // 
+            // cboxVrijemePrikazivanja
+            // 
+            this.cboxVrijemePrikazivanja.FormattingEnabled = true;
+            this.cboxVrijemePrikazivanja.Location = new System.Drawing.Point(121, 172);
+            this.cboxVrijemePrikazivanja.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cboxVrijemePrikazivanja.Name = "cboxVrijemePrikazivanja";
+            this.cboxVrijemePrikazivanja.Size = new System.Drawing.Size(160, 24);
+            this.cboxVrijemePrikazivanja.TabIndex = 15;
+            // 
+            // lblCijena
+            // 
+            this.lblCijena.AutoSize = true;
+            this.lblCijena.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.lblCijena.Location = new System.Drawing.Point(19, 198);
+            this.lblCijena.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCijena.Name = "lblCijena";
+            this.lblCijena.Size = new System.Drawing.Size(79, 25);
+            this.lblCijena.TabIndex = 16;
+            this.lblCijena.Text = "Cijena:";
+            // 
+            // Informacije_o_filmu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(952, 534);
+            this.Controls.Add(this.lblCijena);
+            this.Controls.Add(this.cboxVrijemePrikazivanja);
+            this.Controls.Add(this.Dvorana);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Sadrzaj);
+            this.Controls.Add(this.Naziv);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.ID);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Informacije_o_filmu";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Informacije_o_filmu_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label ID;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label Naziv;
+        private System.Windows.Forms.TextBox Sadrzaj;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label Dvorana;
+        private System.Windows.Forms.ComboBox cboxVrijemePrikazivanja;
+        private System.Windows.Forms.Label lblCijena;
+    }
+}
---
+++ Software/Luka_Zrinski_Pregled_Filmova/Informacije_o_filmu.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test
+{
+    public partial class Informacije_o_filmu : Form
+    {
+        private Filmovi Info;
+        public Informacije_o_filmu(Filmovi info)
+        {
+            Info = info;
+            InitializeComponent();
+        }
+
+        private void Informacije_o_filmu_Load(object sender, EventArgs e)
+        {
+            ID.Text = "" + Info.ID;
+            Naziv.Text = Info.Naziv;
+            cboxVrijemePrikazivanja.Text = "" + Info.Datum;
+            Dvorana.Text = Info.Dvorana;
+        }
+    }
+}
