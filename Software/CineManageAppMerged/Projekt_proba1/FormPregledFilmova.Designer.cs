41ca1561260f13d9b423926e2398e6fd0a7b6ee3
DigitalDevelooper <robert.langus@gmail.com>
2020-07-03 11:44:38 +0200

    Promjena dizajna forme pregled filmova

--- Software/CineManageAppMerged/Projekt_proba1/FormPregledFilmova.Designer.cs
+++ Software/CineManageAppMerged/Projekt_proba1/FormPregledFilmova.Designer.cs
@@ -43,13 +43,13 @@
             this.label1 = new System.Windows.Forms.Label();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.gboxFilter = new System.Windows.Forms.GroupBox();
+            this.btnFilterKategorija = new System.Windows.Forms.Button();
+            this.btnFilterNaziv = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
             this.txtNazivFilter = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.cboxKategorije = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.btnFilterNaziv = new System.Windows.Forms.Button();
-            this.btnFilterKategorija = new System.Windows.Forms.Button();
             this.gboxUrediFilmove.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvFilmovi)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pboxKinoSlika)).BeginInit();
@@ -63,11 +63,9 @@
             this.gboxUrediFilmove.Controls.Add(this.btnAzurirajFilm);
             this.gboxUrediFilmove.Controls.Add(this.btnObrisi);
             this.gboxUrediFilmove.Controls.Add(this.btnPrikazPosovanja);
-            this.gboxUrediFilmove.Location = new System.Drawing.Point(13, 483);
-            this.gboxUrediFilmove.Margin = new System.Windows.Forms.Padding(4);
+            this.gboxUrediFilmove.Location = new System.Drawing.Point(10, 392);
             this.gboxUrediFilmove.Name = "gboxUrediFilmove";
-            this.gboxUrediFilmove.Padding = new System.Windows.Forms.Padding(4);
-            this.gboxUrediFilmove.Size = new System.Drawing.Size(233, 191);
+            this.gboxUrediFilmove.Size = new System.Drawing.Size(175, 155);
             this.gboxUrediFilmove.TabIndex = 23;
             this.gboxUrediFilmove.TabStop = false;
             this.gboxUrediFilmove.Text = "Admin funkcije";
@@ -75,40 +73,36 @@
             // 
             // btnDodajNoviFilm
             // 
-            this.btnDodajNoviFilm.Location = new System.Drawing.Point(13, 32);
-            this.btnDodajNoviFilm.Margin = new System.Windows.Forms.Padding(4);
+            this.btnDodajNoviFilm.Location = new System.Drawing.Point(10, 26);
             this.btnDodajNoviFilm.Name = "btnDodajNoviFilm";
-            this.btnDodajNoviFilm.Size = new System.Drawing.Size(100, 25);
+            this.btnDodajNoviFilm.Size = new System.Drawing.Size(144, 20);
             this.btnDodajNoviFilm.TabIndex = 5;
             this.btnDodajNoviFilm.Text = "Dodaj";
             this.btnDodajNoviFilm.UseVisualStyleBackColor = true;
             // 
             // btnAzurirajFilm
             // 
-            this.btnAzurirajFilm.Location = new System.Drawing.Point(13, 115);
-            this.btnAzurirajFilm.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAzurirajFilm.Location = new System.Drawing.Point(10, 93);
             this.btnAzurirajFilm.Name = "btnAzurirajFilm";
-            this.btnAzurirajFilm.Size = new System.Drawing.Size(100, 25);
+            this.btnAzurirajFilm.Size = new System.Drawing.Size(144, 20);
             this.btnAzurirajFilm.TabIndex = 6;
             this.btnAzurirajFilm.Text = "Ažuriraj";
             this.btnAzurirajFilm.UseVisualStyleBackColor = true;
             // 
             // btnObrisi
             // 
-            this.btnObrisi.Location = new System.Drawing.Point(13, 75);
-            this.btnObrisi.Margin = new System.Windows.Forms.Padding(4);
+            this.btnObrisi.Location = new System.Drawing.Point(10, 61);
             this.btnObrisi.Name = "btnObrisi";
-            this.btnObrisi.Size = new System.Drawing.Size(100, 25);
+            this.btnObrisi.Size = new System.Drawing.Size(144, 20);
             this.btnObrisi.TabIndex = 8;
             this.btnObrisi.Text = "Obriši";
             this.btnObrisi.UseVisualStyleBackColor = true;
             // 
             // btnPrikazPosovanja
             // 
-            this.btnPrikazPosovanja.Location = new System.Drawing.Point(13, 155);
-            this.btnPrikazPosovanja.Margin = new System.Windows.Forms.Padding(4);
+            this.btnPrikazPosovanja.Location = new System.Drawing.Point(10, 126);
             this.btnPrikazPosovanja.Name = "btnPrikazPosovanja";
-            this.btnPrikazPosovanja.Size = new System.Drawing.Size(192, 28);
+            this.btnPrikazPosovanja.Size = new System.Drawing.Size(144, 23);
             this.btnPrikazPosovanja.TabIndex = 19;
             this.btnPrikazPosovanja.Text = "Prikaz poslovanja";
             this.btnPrikazPosovanja.UseVisualStyleBackColor = true;
@@ -118,10 +112,9 @@
             // 
             this.dtPickerDatum.CustomFormat = "\"yyyy-MM-dd hh:mm:ss tt\"";
             this.dtPickerDatum.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dtPickerDatum.Location = new System.Drawing.Point(154, 124);
-            this.dtPickerDatum.Margin = new System.Windows.Forms.Padding(4);
+            this.dtPickerDatum.Location = new System.Drawing.Point(116, 101);
             this.dtPickerDatum.Name = "dtPickerDatum";
-            this.dtPickerDatum.Size = new System.Drawing.Size(201, 22);
+            this.dtPickerDatum.Size = new System.Drawing.Size(152, 20);
             this.dtPickerDatum.TabIndex = 22;
             this.dtPickerDatum.Value = new System.DateTime(2020, 7, 1, 0, 0, 0, 0);
             // 
@@ -131,29 +124,26 @@
             this.lblKorisnickoIme.BackColor = System.Drawing.SystemColors.MenuHighlight;
             this.lblKorisnickoIme.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblKorisnickoIme.ForeColor = System.Drawing.Color.FloralWhite;
-            this.lblKorisnickoIme.Location = new System.Drawing.Point(565, 13);
-            this.lblKorisnickoIme.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblKorisnickoIme.Location = new System.Drawing.Point(482, 11);
             this.lblKorisnickoIme.Name = "lblKorisnickoIme";
-            this.lblKorisnickoIme.Size = new System.Drawing.Size(87, 20);
+            this.lblKorisnickoIme.Size = new System.Drawing.Size(69, 16);
             this.lblKorisnickoIme.TabIndex = 21;
             this.lblKorisnickoIme.Text = "Korisnik id";
             // 
             // dgvFilmovi
             // 
             this.dgvFilmovi.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvFilmovi.Location = new System.Drawing.Point(26, 205);
-            this.dgvFilmovi.Margin = new System.Windows.Forms.Padding(4);
+            this.dgvFilmovi.Location = new System.Drawing.Point(20, 167);
             this.dgvFilmovi.Name = "dgvFilmovi";
             this.dgvFilmovi.RowHeadersWidth = 51;
-            this.dgvFilmovi.Size = new System.Drawing.Size(817, 252);
+            this.dgvFilmovi.Size = new System.Drawing.Size(613, 205);
             this.dgvFilmovi.TabIndex = 20;
             // 
             // btnFilterDatum
             // 
-            this.btnFilterDatum.Location = new System.Drawing.Point(383, 123);
-            this.btnFilterDatum.Margin = new System.Windows.Forms.Padding(4);
+            this.btnFilterDatum.Location = new System.Drawing.Point(287, 100);
             this.btnFilterDatum.Name = "btnFilterDatum";
-            this.btnFilterDatum.Size = new System.Drawing.Size(156, 28);
+            this.btnFilterDatum.Size = new System.Drawing.Size(117, 23);
             this.btnFilterDatum.TabIndex = 18;
             this.btnFilterDatum.Text = "Filtriraj po datumu";
             this.btnFilterDatum.UseVisualStyleBackColor = true;
@@ -161,10 +151,9 @@
             // 
             // btnPocetniPrikaz
             // 
-            this.btnPocetniPrikaz.Location = new System.Drawing.Point(201, 164);
-            this.btnPocetniPrikaz.Margin = new System.Windows.Forms.Padding(4);
+            this.btnPocetniPrikaz.Location = new System.Drawing.Point(151, 133);
             this.btnPocetniPrikaz.Name = "btnPocetniPrikaz";
-            this.btnPocetniPrikaz.Size = new System.Drawing.Size(144, 28);
+            this.btnPocetniPrikaz.Size = new System.Drawing.Size(108, 23);
             this.btnPocetniPrikaz.TabIndex = 17;
             this.btnPocetniPrikaz.Text = "Očisti filtere";
             this.btnPocetniPrikaz.UseVisualStyleBackColor = true;
@@ -174,20 +163,18 @@
             // 
             this.btnOdjava.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.btnOdjava.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnOdjava.Location = new System.Drawing.Point(667, 9);
-            this.btnOdjava.Margin = new System.Windows.Forms.Padding(4);
+            this.btnOdjava.Location = new System.Drawing.Point(558, 7);
             this.btnOdjava.Name = "btnOdjava";
-            this.btnOdjava.Size = new System.Drawing.Size(99, 28);
+            this.btnOdjava.Size = new System.Drawing.Size(74, 23);
             this.btnOdjava.TabIndex = 16;
             this.btnOdjava.Text = "Odjava";
             this.btnOdjava.UseVisualStyleBackColor = false;
             // 
             // pboxKinoSlika
             // 
-            this.pboxKinoSlika.Location = new System.Drawing.Point(26, 66);
-            this.pboxKinoSlika.Margin = new System.Windows.Forms.Padding(4);
+            this.pboxKinoSlika.Location = new System.Drawing.Point(20, 54);
             this.pboxKinoSlika.Name = "pboxKinoSlika";
-            this.pboxKinoSlika.Size = new System.Drawing.Size(119, 108);
+            this.pboxKinoSlika.Size = new System.Drawing.Size(89, 88);
             this.pboxKinoSlika.TabIndex = 15;
             this.pboxKinoSlika.TabStop = false;
             // 
@@ -195,20 +182,18 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(166, 103);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(124, 84);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(313, 36);
+            this.label1.Size = new System.Drawing.Size(250, 29);
             this.label1.TabIndex = 14;
             this.label1.Text = "PREGLED FILMOVA";
             // 
             // pictureBox2
             // 
             this.pictureBox2.BackColor = System.Drawing.SystemColors.MenuHighlight;
-            this.pictureBox2.Location = new System.Drawing.Point(7, 2);
-            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox2.Location = new System.Drawing.Point(-6, -6);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(787, 46);
+            this.pictureBox2.Size = new System.Drawing.Size(671, 45);
             this.pictureBox2.TabIndex = 24;
             this.pictureBox2.TabStop = false;
             // 
@@ -224,82 +209,87 @@
             this.gboxFilter.Controls.Add(this.label2);
             this.gboxFilter.Controls.Add(this.btnFilterDatum);
             this.gboxFilter.Controls.Add(this.btnPocetniPrikaz);
-            this.gboxFilter.Location = new System.Drawing.Point(271, 474);
+            this.gboxFilter.Location = new System.Drawing.Point(203, 385);
+            this.gboxFilter.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.gboxFilter.Name = "gboxFilter";
-            this.gboxFilter.Size = new System.Drawing.Size(572, 200);
+            this.gboxFilter.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.gboxFilter.Size = new System.Drawing.Size(429, 162);
             this.gboxFilter.TabIndex = 25;
             this.gboxFilter.TabStop = false;
             this.gboxFilter.Text = "Filtriranje";
             // 
+            // btnFilterKategorija
+            // 
+            this.btnFilterKategorija.Location = new System.Drawing.Point(287, 28);
+            this.btnFilterKategorija.Name = "btnFilterKategorija";
+            this.btnFilterKategorija.Size = new System.Drawing.Size(117, 23);
+            this.btnFilterKategorija.TabIndex = 25;
+            this.btnFilterKategorija.Text = "Filtriraj po kategoriji";
+            this.btnFilterKategorija.UseVisualStyleBackColor = true;
+            this.btnFilterKategorija.Click += new System.EventHandler(this.btnFilterKategorija_Click);
+            // 
+            // btnFilterNaziv
+            // 
+            this.btnFilterNaziv.Location = new System.Drawing.Point(287, 63);
+            this.btnFilterNaziv.Name = "btnFilterNaziv";
+            this.btnFilterNaziv.Size = new System.Drawing.Size(117, 23);
+            this.btnFilterNaziv.TabIndex = 24;
+            this.btnFilterNaziv.Text = "Filtriraj po nazivu";
+            this.btnFilterNaziv.UseVisualStyleBackColor = true;
+            this.btnFilterNaziv.Click += new System.EventHandler(this.btnFilterNaziv_Click);
+            // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(6, 129);
+            this.label4.Location = new System.Drawing.Point(10, 105);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(128, 17);
+            this.label4.Size = new System.Drawing.Size(97, 13);
             this.label4.TabIndex = 23;
             this.label4.Text = "Datum prikazivanja";
             // 
             // txtNazivFilter
             // 
-            this.txtNazivFilter.Location = new System.Drawing.Point(154, 81);
+            this.txtNazivFilter.Location = new System.Drawing.Point(116, 66);
+            this.txtNazivFilter.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtNazivFilter.Name = "txtNazivFilter";
-            this.txtNazivFilter.Size = new System.Drawing.Size(217, 22);
+            this.txtNazivFilter.Size = new System.Drawing.Size(164, 20);
             this.txtNazivFilter.TabIndex = 22;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(-1, 84);
+            this.label3.Location = new System.Drawing.Point(5, 68);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(135, 17);
+            this.label3.Size = new System.Drawing.Size(103, 13);
             this.label3.TabIndex = 21;
             this.label3.Text = "Naziv filma/redatelja";
             // 
             // cboxKategorije
             // 
             this.cboxKategorije.FormattingEnabled = true;
-            this.cboxKategorije.Location = new System.Drawing.Point(154, 38);
+            this.cboxKategorije.Location = new System.Drawing.Point(116, 31);
+            this.cboxKategorije.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.cboxKategorije.Name = "cboxKategorije";
-            this.cboxKategorije.Size = new System.Drawing.Size(140, 24);
+            this.cboxKategorije.Size = new System.Drawing.Size(106, 21);
             this.cboxKategorije.TabIndex = 20;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(62, 41);
+            this.label2.Location = new System.Drawing.Point(52, 33);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(72, 17);
+            this.label2.Size = new System.Drawing.Size(54, 13);
             this.label2.TabIndex = 19;
             this.label2.Text = "Kategorija";
             // 
-            // btnFilterNaziv
-            // 
-            this.btnFilterNaziv.Location = new System.Drawing.Point(383, 78);
-            this.btnFilterNaziv.Margin = new System.Windows.Forms.Padding(4);
-            this.btnFilterNaziv.Name = "btnFilterNaziv";
-            this.btnFilterNaziv.Size = new System.Drawing.Size(156, 28);
-            this.btnFilterNaziv.TabIndex = 24;
-            this.btnFilterNaziv.Text = "Filtriraj po nazivu";
-            this.btnFilterNaziv.UseVisualStyleBackColor = true;
-            this.btnFilterNaziv.Click += new System.EventHandler(this.btnFilterNaziv_Click);
-            // 
-            // btnFilterKategorija
-            // 
-            this.btnFilterKategorija.Location = new System.Drawing.Point(383, 34);
-            this.btnFilterKategorija.Margin = new System.Windows.Forms.Padding(4);
-            this.btnFilterKategorija.Name = "btnFilterKategorija";
-            this.btnFilterKategorija.Size = new System.Drawing.Size(156, 28);
-            this.btnFilterKategorija.TabIndex = 25;
-            this.btnFilterKategorija.Text = "Filtriraj po kategoriji";
-            this.btnFilterKategorija.UseVisualStyleBackColor = true;
-            this.btnFilterKategorija.Click += new System.EventHandler(this.btnFilterKategorija_Click);
-            // 
             // FormPregledFilmova
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(865, 686);
+            this.ClientSize = new System.Drawing.Size(649, 557);
             this.Controls.Add(this.gboxUrediFilmove);
             this.Controls.Add(this.lblKorisnickoIme);
             this.Controls.Add(this.dgvFilmovi);
@@ -308,6 +298,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.gboxFilter);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "FormPregledFilmova";
             this.Text = "FormPregledFilmova";
             this.Load += new System.EventHandler(this.FormPregledFilmova_Load);
